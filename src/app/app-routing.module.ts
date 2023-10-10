36ce51949627755e8d7b65865c574580f0fa6816
felix-lpz <i1921286@continental.edu.pe>
2023-10-09 21:38:55 -0500

    Rutas Basicas y definicion paginas principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './page/welcome/welcome.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //* Ruteo Simple
+  {
+    path: '',component:WelcomeComponent
+  },
+  {
+    path: '404',component: NotFoundComponent
+  }
+  //* Vamo a utilizar a lazy loading
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WelcomeComponent } from './page/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
